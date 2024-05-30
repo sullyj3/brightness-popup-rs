9b1f03fcfd93576c252f21ebde3c7d636ba5be76
James Sully <sullyj3@gmail.com>
2024-05-30 20:27:56 +1000

    switch from mpsc to signal, cancel threads on window close

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use async_scoped;
 use blight;
+use dirs;
 use eframe::egui;
 use egui::ViewportBuilder;
 use fs2::FileExt;
+use futures_signals::signal::{self, SignalExt};
 use std::fs::File;
 use std::io::Result;
 use std::path::PathBuf;
-use std::sync::mpsc;
 use tokio;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{UnixListener, UnixStream};
-use dirs;
 
 const PROG_NAME: &str = "brightness-slider";
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let runtime_dir = dirs::runtime_dir()
-            .expect("Failed to get runtime dir")
-            .join(PROG_NAME);
+        .expect("Failed to get runtime dir")
+        .join(PROG_NAME);
 
     std::fs::create_dir_all(&runtime_dir)?;
 
@@ -59,17 +59,18 @@ fn write_brightness_to_device(
     device.write_value(value)
 }
 
-async fn device_write_thread(rx: mpsc::Receiver<u8>, mut device: blight::Device) -> Result<()> {
-    loop {
-        let Ok(target_brightness) = rx.recv() else {
-            break;
-        };
+async fn device_write_thread(
+    rx: signal::MutableSignal<u8>,
+    mut device: blight::Device,
+) -> Result<()> {
+    rx.for_each(|target_brightness| {
         write_brightness_to_device(&mut device, target_brightness)
             .expect("Failed to write brightness");
-    }
+        async {}
+    })
+    .await;
 
-    // gui has closed, report final brightness and exit
-    device.reload();
+    // gui has closed, exit
     Ok(())
 }
 
@@ -78,8 +79,8 @@ async fn server_thread(socket_path: &PathBuf) -> Result<()> {
 
     // Ensure no stale socket file
     match std::fs::remove_file(socket_path) {
-        Ok(_) => {},
-        Err(e) if e.kind() == std::io::ErrorKind::NotFound => {},
+        Ok(_) => {}
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => {}
         Err(e) => return Err(e),
     }
 
@@ -109,45 +110,31 @@ async fn brightness_slider(socket_path: PathBuf) {
         let curr_brightness: u8 = device.current_percent().round() as u8;
         println!("Initial brightness: {}", curr_brightness);
 
-        // TODO: consider using tokio::sync::watch::channel.
-        // This drops all values except the most recent one, which is what we want.
-        // https://docs.rs/tokio/latest/tokio/sync/watch/fn.channel.html
-        // another possibility is `Eventual`
-        // https://docs.rs/crate/eventuals/0.6.7
-        //
-        // comparison of watch and eventual from the author of eventual:
-        // https://www.reddit.com/r/rust/comments/oo917a/comment/h6ygv60/
-        //
-        // another comment mentions that eventuals are like FRP signals
-        // see also https://lib.rs/crates/futures-signals (more stars + more recent commits)
-        let (tx_brightness, rx_brightness) = mpsc::channel();
+        let brightness = signal::Mutable::new(curr_brightness);
 
-        // TODO ensure these are cancelled when the egui window is closed
-        s.spawn(device_write_thread(rx_brightness, device));
-        s.spawn(server_thread(&socket_path));
+        s.spawn_cancellable(device_write_thread(brightness.signal(), device), || Ok(()));
+        s.spawn_cancellable(server_thread(&socket_path), || Ok(()));
 
-        let app = BrightnessApp::new(tx_brightness, curr_brightness);
+        let app = BrightnessApp::new(brightness);
         eframe::run_native("Brightness", window_options, Box::new(|_cc| Box::new(app))).unwrap();
+        s.cancel();
     });
 }
 
 #[derive(Debug)]
 struct BrightnessApp {
     // percentage
-    target_brightness: u8,
-    tx_brightness: mpsc::Sender<u8>,
+    brightness: signal::Mutable<u8>,
 }
 
 impl BrightnessApp {
-    fn new(tx_brightness: mpsc::Sender<u8>, curr_brightness: u8) -> Self {
-        Self {
-            target_brightness: curr_brightness,
-            tx_brightness,
-        }
+    fn new(brightness: signal::Mutable<u8>) -> Self {
+        Self { brightness }
     }
 
     fn add_target_brightness(&mut self, delta: i16) {
-        self.target_brightness = (self.target_brightness as i16 + delta).clamp(0, 100) as u8;
+        self.brightness
+            .replace_with(|b| (*b as i16 + delta).clamp(0, 100) as u8);
     }
 
     fn handle_input(&mut self, ctx: &egui::Context) {
@@ -177,17 +164,18 @@ impl BrightnessApp {
 impl eframe::App for BrightnessApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.add(
-                // todo: try Slider::from_get_set
-                egui::Slider::new(&mut self.target_brightness, 0..=100)
-                    .show_value(false)
-                    .vertical(),
-            );
+            let slider = egui::Slider::from_get_set(0.0..=100.0, |v| {
+                if let Some(v) = v {
+                    self.brightness.set(v.round() as u8);
+                }
+                self.brightness.get() as f64
+            })
+            .show_value(false)
+            .vertical();
+
+            ui.add(slider);
 
             self.handle_input(ctx);
-            self.tx_brightness
-                .send(self.target_brightness)
-                .expect("Bg thread hung up unexpectedly");
         });
     }
 }
